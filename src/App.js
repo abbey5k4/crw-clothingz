217e04833594aaa569666c63b6e0d2b8c461d771
adetayo <adetayoabiodun4@gmail.com>
2021-07-09 18:31:19 +0100

    navigayion using react router withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Route, Switch } from "react-router-dom" 
 
 function App() {
+  const HatsPage = () => {
+    return (
+      <div>
+        HATS PAGE
+      </div>
+    )
+  }
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
