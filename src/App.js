f2834be07a97f51d0707cb16feb075a4906a860c
adetayo <adetayoabiodun4@gmail.com>
2021-07-10 18:03:01 +0100

    implented firebase utils and stored users

--- src/App.js
+++ src/App.js
@@ -22,20 +22,20 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      // if (userAuth) {
-      //   const userRef = await createUserProfileDocument(userAuth);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      //   userRef.onSnapshot(snapShot => {
-      //     this.setState({
-      //       currentUser: {
-      //         id: snapShot.id,
-      //         ...snapShot.data()
-      //       }
-      //     });
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
 
-      //     console.log(this.state);
-      //   });
-      // }
+          console.log(this.state);
+        });
+      }
 
       this.setState({ currentUser: userAuth });
     });
